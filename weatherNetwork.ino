41a57ce6dbd1f7d8af844a3e004a8bf3c5a4443f
Andreas Lie <ael@datarespons.no>
2017-06-06 15:22:21 +0200

    Formatted codebase with clang-format

--- weatherNetwork.ino
+++ weatherNetwork.ino
@@ -1,40 +1,40 @@
+#include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
-#include <ESP8266HTTPClient.h>
 #include <WiFiUdp.h>
 
 #define NETWORK_DR
 #ifdef NETWORK_DR
-const char* ssid = "datarespons";
+const char* ssid     = "datarespons";
 const char* password = "DRwireless";
 #endif
 
 ESP8266WiFiMulti WiFiMulti;
 HTTPClient http;
-WiFiUDP UDP;                     // Create an instance of the WiFiUDP class to send and receive
+WiFiUDP UDP; // Create an instance of the WiFiUDP class to send and receive
 IPAddress timeServerIP;
 const char* NTPServerName = "no.pool.ntp.org";
 const int NTP_PACKET_SIZE = 48;  // NTP time stamp is in the first 48 bytes of the message
 byte NTPBuffer[NTP_PACKET_SIZE]; // buffer to hold incoming and outgoing packets
 
 // ntp
-unsigned long intervalNTP = 60000; // Request NTP time every minute
-unsigned long prevNTP = 0;
+unsigned long intervalNTP     = 60000; // Request NTP time every minute
+unsigned long prevNTP         = 0;
 unsigned long lastNTPResponse = 0;
-uint32_t timeUNIX = 0;
-unsigned long prevActualTime = 0;
-unsigned long currentMillis = 0;
+uint32_t timeUNIX             = 0;
+unsigned long prevActualTime  = 0;
+unsigned long currentMillis   = 0;
 
 // current time
-volatile int m_currentHour   = 0;
-volatile int m_currentMinute = 0;
+volatile int m_currentHour      = 0;
+volatile int m_currentMinute    = 0;
 volatile int m_everyOtherMinute = 0;
 
 void sendNTPpacket()
 {
-    memset(NTPBuffer, 0, NTP_PACKET_SIZE);  // set all bytes in the buffer to 0
+    memset(NTPBuffer, 0, NTP_PACKET_SIZE); // set all bytes in the buffer to 0
     // Initialize values needed to form NTP request
-    NTPBuffer[0] = 0b11100011;   // LI, Version, Mode
+    NTPBuffer[0] = 0b11100011; // LI, Version, Mode
     // send a packet requesting a timestamp:
     UDP.beginPacket(timeServerIP, 123); // NTP requests are to port 123
     UDP.write(NTPBuffer, NTP_PACKET_SIZE);
@@ -46,30 +46,30 @@ void networkSetup()
     WiFiMulti.addAP(ssid, password);
     UDP.begin(123);
 
-    if(!WiFi.hostByName(NTPServerName, timeServerIP)) // Get the IP address of the NTP server
+    if (!WiFi.hostByName(NTPServerName, timeServerIP)) // Get the IP address of the NTP server
     {
         Serial.println("DNS lookup failed. Rebooting.");
         Serial.flush();
         ESP.reset();
     }
-    
+
     Serial.print("Time server IP:\t");
     Serial.println(timeServerIP);
-  
+
     Serial.println("\r\nSending NTP request ...");
-    sendNTPpacket();  
+    sendNTPpacket();
 }
 
-void transmitDataToWeatherUnderground(String & wuAddressWithData)
+void transmitDataToWeatherUnderground(String& wuAddressWithData)
 {
-    if((WiFiMulti.run() == WL_CONNECTED))
+    if ((WiFiMulti.run() == WL_CONNECTED))
     {
         http.begin(wuAddressWithData, "12 DB BB 24 8E 0F 6F D4 63 EC 45 DD 5B ED 37 D7 6F B1 5F E5"); // wunderground ssl fingerprint
         int httpCode = http.GET();
 
-        if(httpCode > 0)
+        if (httpCode > 0)
         {
-            if(httpCode == HTTP_CODE_OK)
+            if (httpCode == HTTP_CODE_OK)
             {
                 String payload = http.getString();
                 Serial.println(payload);
@@ -87,15 +87,15 @@ void transmitDataToWeatherUnderground(String & wuAddressWithData)
 void checkForNtpUpdate()
 {
     currentMillis = millis();
-  
-    if (currentMillis - prevNTP > intervalNTP)   // If a minute has passed since last NTP request
-    { 
+
+    if (currentMillis - prevNTP > intervalNTP) // If a minute has passed since last NTP request
+    {
         prevNTP = currentMillis;
         Serial.println("\r\nSending NTP request ...");
         sendNTPpacket();
     }
 
-    uint32_t time = getTime();                   // Check if an NTP response has arrived and get the (UNIX) time
+    uint32_t time = getTime(); // Check if an NTP response has arrived and get the (UNIX) time
 
     if (time) // If a new timestamp has been received
     {
@@ -116,11 +116,11 @@ void checkForNtpUpdate()
 
 uint32_t getTime()
 {
-    if (UDP.parsePacket() == 0) 
+    if (UDP.parsePacket() == 0)
     { // If there's no response (yet)
-      return 0;
+        return 0;
     }
-  
+
     UDP.read(NTPBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
     // Combine the 4 timestamp bytes into one 32-bit number
     uint32_t NTPTime = (NTPBuffer[40] << 24) | (NTPBuffer[41] << 16) | (NTPBuffer[42] << 8) | NTPBuffer[43];
@@ -134,19 +134,19 @@ uint32_t getTime()
 
 bool reportNewHour()
 {
-    int nowMinute = getMinutes(getUnixTime());
+    int nowMinute     = getMinutes(getUnixTime());
     bool timeRollover = false;
 
     if (nowMinute < m_currentMinute)
         timeRollover = true;
-          
+
     m_currentMinute = nowMinute;
     return timeRollover;
 }
 
 bool reportNewDay()
 {
-    int nowHour = getHours(getUnixTime());
+    int nowHour       = getHours(getUnixTime());
     bool timeRollover = false;
 
     if (nowHour < m_currentHour)
@@ -158,7 +158,7 @@ bool reportNewDay()
 
 bool reportEveryOtherMinute()
 {
-    int nowMinute = getMinutes(getUnixTime());
+    int nowMinute     = getMinutes(getUnixTime());
     bool timeRollover = false;
 
     if ((nowMinute % 2 == 0) && nowMinute != m_everyOtherMinute)
@@ -181,9 +181,18 @@ void printTime()
 inline uint32_t getUnixTime()
 {
     currentMillis = millis();
-    return (uint32_t)(timeUNIX + (currentMillis - lastNTPResponse)/1000);
+    return (uint32_t)(timeUNIX + (currentMillis - lastNTPResponse) / 1000);
 }
 
-inline int getSeconds(uint32_t UNIXTime)  { return UNIXTime % 60; }
-inline int getMinutes(uint32_t UNIXTime)  { return UNIXTime / 60 % 60; }
-inline int getHours(uint32_t UNIXTime)    { return UNIXTime / 3600 % 24; }
+inline int getSeconds(uint32_t UNIXTime)
+{
+    return UNIXTime % 60;
+}
+inline int getMinutes(uint32_t UNIXTime)
+{
+    return UNIXTime / 60 % 60;
+}
+inline int getHours(uint32_t UNIXTime)
+{
+    return UNIXTime / 3600 % 24;
+}
--- weatherStation.ino
+++ weatherStation.ino
@@ -1,88 +1,93 @@
-#include <SFE_BMP180.h>
-#include <Wire.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <SFE_BMP180.h>
+#include <Wire.h>
 #include <math.h>
 
 // enable / disable subsystems
 const int isAnemometerActive = 1;
-const int isRainmeterActive = 1;
-const int isWindvaneActive = 1;
-const int isDhtActive = 1;
-const int isBmpActive = 1;
+const int isRainmeterActive  = 1;
+const int isWindvaneActive   = 1;
+const int isDhtActive        = 1;
+const int isBmpActive        = 1;
 
 // direction for vane
 // S, SW, W, NW, N, NE, E, SE
 const int windVaneOffset = 0;
 
 // pin definitions
-const int anemometerPin     = 14;
-const int precipitationPin  = 13;
-const int sdaPin            = 4;
-const int sclPin            = 5;
-const int windvanePin       = A0;
-const int redLedPin         = 0;
+const int anemometerPin    = 14;
+const int precipitationPin = 13;
+const int sdaPin           = 4;
+const int sclPin           = 5;
+const int windvanePin      = A0;
+const int redLedPin        = 0;
 
-#define DHTPIN 12         // what digital pin we're connected to
-#define DHTTYPE DHT22     // DHT 22  (AM2302), AM2321
+#define DHTPIN 12     // what digital pin we're connected to
+#define DHTTYPE DHT22 // DHT 22  (AM2302), AM2321
 DHT dht(DHTPIN, DHTTYPE);
 
 // timekeeping
-unsigned long lastNtpCheckTime        = 0;
-const unsigned long thresholdTimeNtp  = 10000; // ten sec threshold
+unsigned long lastNtpCheckTime       = 0;
+const unsigned long thresholdTimeNtp = 10000; // ten sec threshold
 
 volatile unsigned long previousWindInterruptTime = 0;
 volatile unsigned long previousRainInterruptTime = 0;
-volatile unsigned long previousGustResetTime = 0;
+volatile unsigned long previousGustResetTime     = 0;
 
 // counters used in the ISR's
-volatile int windCounter = 0;
+volatile int windCounter       = 0;
 volatile int rainCounterHourly = 0;
-volatile int rainCounterDaily = 0;
-volatile int vaneDirection = 0;
-volatile int gustMaxRecorded = 0;
-volatile int gustCounter = 0;
+volatile int rainCounterDaily  = 0;
+volatile int vaneDirection     = 0;
+volatile int gustMaxRecorded   = 0;
+volatile int gustCounter       = 0;
 
-volatile boolean redLedState  = true;
+volatile boolean redLedState = true;
 
-const unsigned int interruptSleep = 2;         // milliseconds delay to avoid multiple readings
+const unsigned int interruptSleep       = 2;   // milliseconds delay to avoid multiple readings
 const unsigned long windFeedbackSeconds = 120; // every other minute
 
-const float windSpeed = 0.666667f;                  // 2.4 km/h == 0.667 m/s
-const float windSpeedMph = 1.4913;                  // 2.4 km/h == 0.667 m/s == 1.4913 m/h
-const float rainSpeedInch = 0.0011f;                // 0.2794 mm precipitation on every bucket emptying
-const float rainSpeedMm   = 0.2794f;                // 0.2794 mm precipitation on every bucket emptying
+const float windSpeed     = 0.666667f; // 2.4 km/h == 0.667 m/s
+const float windSpeedMph  = 1.4913;    // 2.4 km/h == 0.667 m/s == 1.4913 m/h
+const float rainSpeedInch = 0.0011f;   // 0.2794 mm precipitation on every bucket emptying
+const float rainSpeedMm   = 0.2794f;   // 0.2794 mm precipitation on every bucket emptying
 
 const String wuBegin = "https://weatherstation.wunderground.com/weatherstation/updateweatherstation.php?ID=IHORDALA133&PASSWORD=MXLVDW6X&dateutc=now";
-const String wuEnd = "&action=updateraw";
+const String wuEnd   = "&action=updateraw";
 
-const String wuTemp       = "&tempf=";         // ok
-const String wuHumid      = "&humidity=";      // ok
-const String wuDewpt      = "&dewptf=";        // ok
-const String wuRain       = "&rainin=";        // ok
-const String wuDailyRain  = "&dailyrainin=";   // ok
-const String wuBaro       = "&baromin=";       // ok
-const String wuBaroTemp   = "&temp2f=";        // ok
-const String wuWinddir    = "&winddir=";       // ok
-const String wuWindspeed  = "&windspdmph=";     // ok
-const String wuWindspeed2m  = "&windspdmph_avg2m=";  // ok
-const String wuGustspeed  = "&windgustmph=";   // ok
+const String wuTemp        = "&tempf=";            // ok
+const String wuHumid       = "&humidity=";         // ok
+const String wuDewpt       = "&dewptf=";           // ok
+const String wuRain        = "&rainin=";           // ok
+const String wuDailyRain   = "&dailyrainin=";      // ok
+const String wuBaro        = "&baromin=";          // ok
+const String wuBaroTemp    = "&temp2f=";           // ok
+const String wuWinddir     = "&winddir=";          // ok
+const String wuWindspeed   = "&windspdmph=";       // ok
+const String wuWindspeed2m = "&windspdmph_avg2m="; // ok
+const String wuGustspeed   = "&windgustmph=";      // ok
 
 SFE_BMP180 pressure;
 
 #define ALTITUDE 56.0 // Fantoftneset 4
 
-enum REPORT {EVERY_OTHER_MINUTE, HOURLY, DAILY};
-
-String floatToString(float & floatval, int precision)
+enum REPORT
 {
-    int intval = (int)floatval; // downcast
+    EVERY_OTHER_MINUTE,
+    HOURLY,
+    DAILY
+};
+
+String floatToString(float& floatval, int precision)
+{
+    int intval     = (int)floatval; // downcast
     float decimals = floatval - (float)intval;
 
     int power = pow(10, precision);
 
     decimals *= power;
-    int intdecimals = (int) decimals; // downcast
+    int intdecimals = (int)decimals; // downcast
 
     String s;
     s += intval;
@@ -108,28 +113,28 @@ void toggleLedRed()
 
 void setup()
 {
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-  if (pressure.begin())
-      Serial.println("BMP180 init success");
-  else
-      Serial.println("BMP180 init fail\n\n");
+    if (pressure.begin())
+        Serial.println("BMP180 init success");
+    else
+        Serial.println("BMP180 init fail\n\n");
 
-  pinMode(redLedPin, OUTPUT);
-  pinMode(redLedPin, redLedState);
-  digitalWrite(anemometerPin, HIGH);
-  pinMode(windvanePin, INPUT);
-  pinMode(anemometerPin, INPUT_PULLUP);
-  pinMode(precipitationPin, INPUT_PULLUP);
-  pinMode(DHTPIN, INPUT_PULLUP);
+    pinMode(redLedPin, OUTPUT);
+    pinMode(redLedPin, redLedState);
+    digitalWrite(anemometerPin, HIGH);
+    pinMode(windvanePin, INPUT);
+    pinMode(anemometerPin, INPUT_PULLUP);
+    pinMode(precipitationPin, INPUT_PULLUP);
+    pinMode(DHTPIN, INPUT_PULLUP);
 
-  dht.begin();
-  
-  attachInterrupt(anemometerPin, windInterrupt, FALLING);
-  attachInterrupt(precipitationPin, rainInterrupt, FALLING);
+    dht.begin();
 
-  toggleLedRed();
-  networkSetup();
+    attachInterrupt(anemometerPin, windInterrupt, FALLING);
+    attachInterrupt(precipitationPin, rainInterrupt, FALLING);
+
+    toggleLedRed();
+    networkSetup();
 }
 
 void transmitWeatherData(REPORT report)
@@ -147,9 +152,9 @@ void transmitWeatherData(REPORT report)
 
     if (isAnemometerActive == 1) // every other minute
     {
-        float windAmount = (windSpeed * (((float)windCounter) / 2.0f) / (float)windFeedbackSeconds);
+        float windAmount    = (windSpeed * (((float)windCounter) / 2.0f) / (float)windFeedbackSeconds);
         float windAmountMph = (windSpeedMph * (((float)windCounter) / 2.0f) / (float)windFeedbackSeconds);
-        float windGustMph = (windSpeedMph * ((float)gustMaxRecorded) / 2.0f);
+        float windGustMph   = (windSpeedMph * ((float)gustMaxRecorded) / 2.0f);
 
         Serial.print("WindGust: ");
         Serial.print(windGustMph);
@@ -163,7 +168,7 @@ void transmitWeatherData(REPORT report)
         Serial.print(windAmount);
         Serial.println(" ms");
 
-        windCounter = 0; // reset
+        windCounter     = 0; // reset
         gustMaxRecorded = 0; // reset
 
         wuString += wuGustspeed;
@@ -182,8 +187,8 @@ void transmitWeatherData(REPORT report)
     {
         if (report == HOURLY || report == DAILY) // implies the same
         {
-            float rainAmountInch  = rainSpeedInch  * (float)rainCounterHourly;
-            float rainAmountMm    = rainSpeedMm    * (float)rainCounterHourly;
+            float rainAmountInch = rainSpeedInch * (float)rainCounterHourly;
+            float rainAmountMm   = rainSpeedMm * (float)rainCounterHourly;
             Serial.print("Rain mm: ");
             Serial.print(floatToString(rainAmountMm, 4));
             Serial.print("\tRain Inches: ");
@@ -193,24 +198,24 @@ void transmitWeatherData(REPORT report)
             rainCounterHourly = 0; // reset
         }
 
-        float rainAmountInchDaily  = rainSpeedInch  * (float)rainCounterDaily;
+        float rainAmountInchDaily = rainSpeedInch * (float)rainCounterDaily;
         wuString += wuDailyRain;
         wuString += floatToString(rainAmountInchDaily, 4);
 
         if (report == DAILY)
         {
-            rainCounterDaily = 0;  // reset
+            rainCounterDaily = 0; // reset
         }
     }
 
     if (isDhtActive == 1)
     {
         float humidity = dht.readHumidity();
-        float tempc = dht.readTemperature();      // Read temperature as Celsius (the default)
-        float tempf = dht.readTemperature(true);  // Read temperature as Fahrenheit (isFahrenheit = true)
-        float dew = 237.7f * (17.27f * tempc/(237.7f + tempc) + log(humidity/100.0f)) / (17.27f - (17.27f * tempc/(237.7f + tempc)+ log(humidity/100.0f)));
-        dew = dew * (9.0f/5.0f) + 32.0f; // to fahrenheit
-        
+        float tempc    = dht.readTemperature();     // Read temperature as Celsius (the default)
+        float tempf    = dht.readTemperature(true); // Read temperature as Fahrenheit (isFahrenheit = true)
+        float dew      = 237.7f * (17.27f * tempc / (237.7f + tempc) + log(humidity / 100.0f)) / (17.27f - (17.27f * tempc / (237.7f + tempc) + log(humidity / 100.0f)));
+        dew            = dew * (9.0f / 5.0f) + 32.0f; // to fahrenheit
+
         Serial.print("Humidity: ");
         Serial.print(humidity);
         Serial.print(" %\t");
@@ -225,7 +230,7 @@ void transmitWeatherData(REPORT report)
         wuString += wuHumid;
         wuString += humidity;
         wuString += wuTemp;
-        wuString += tempf;      
+        wuString += tempf;
         wuString += wuDewpt;
         wuString += dew;
     }
@@ -233,7 +238,7 @@ void transmitWeatherData(REPORT report)
     if (isBmpActive == 1)
     {
         char status;
-        double T,P,p0,a;
+        double T, P, p0, a;
 
         status = pressure.startTemperature();
 
@@ -246,48 +251,52 @@ void transmitWeatherData(REPORT report)
             {
                 // Print out the measurement:
                 Serial.print("temperature: ");
-                Serial.print(T,2);
+                Serial.print(T, 2);
                 Serial.print(" deg C, ");
-                Serial.print((9.0/5.0)*T+32.0,2);
+                Serial.print((9.0 / 5.0) * T + 32.0, 2);
                 Serial.println(" deg F");
 
                 status = pressure.startPressure(3);
                 if (status != 0)
                 {
                     delay(status);
-                    status = pressure.getPressure(P,T);
+                    status = pressure.getPressure(P, T);
 
                     if (status != 0)
                     {
                         // Print out the measurement:
                         Serial.print("absolute pressure: ");
-                        Serial.print(P,2);
+                        Serial.print(P, 2);
                         Serial.print(" mb, ");
-                        Serial.print(P*0.0295333727,2);
+                        Serial.print(P * 0.0295333727, 2);
                         Serial.println(" inHg");
 
-                        p0 = pressure.sealevel(P,ALTITUDE);
+                        p0 = pressure.sealevel(P, ALTITUDE);
                         Serial.print("relative (sea-level) pressure: ");
-                        Serial.print(p0,2);
+                        Serial.print(p0, 2);
                         Serial.print(" mb, ");
-                        Serial.print(p0*0.0295333727,2);
+                        Serial.print(p0 * 0.0295333727, 2);
                         Serial.println(" inHg");
 
-                        float fp0 = (float)(p0*0.0295333727);
-                        float tf  = (9.0f/5.0f)*(float)T+32.0f;
+                        float fp0 = (float)(p0 * 0.0295333727);
+                        float tf  = (9.0f / 5.0f) * (float)T + 32.0f;
 
                         wuString += wuBaro;
                         wuString += floatToString(fp0, 2);
                         wuString += wuBaroTemp;
                         wuString += floatToString(tf, 2);
                     }
-                    else Serial.println("error retrieving pressure measurement\n");
+                    else
+                        Serial.println("error retrieving pressure measurement\n");
                 }
-                else Serial.println("error starting pressure measurement\n");
+                else
+                    Serial.println("error starting pressure measurement\n");
             }
-            else Serial.println("error retrieving temperature measurement\n");
+            else
+                Serial.println("error retrieving temperature measurement\n");
         }
-        else Serial.println("error starting temperature measurement\n");
+        else
+            Serial.println("error starting temperature measurement\n");
     }
 
     wuString += wuEnd;
@@ -298,24 +307,24 @@ void transmitWeatherData(REPORT report)
 
 int mapVaneDirection()
 {
-    int vaneValue = analogRead(windvanePin);
+    int vaneValue            = analogRead(windvanePin);
     int vaneDirectionDegrees = 0;
 
-    if (vaneValue > 800)        // north
+    if (vaneValue > 800) // north
         vaneDirectionDegrees = 0;
-    else if (vaneValue > 740)   // north east
+    else if (vaneValue > 740) // north east
         vaneDirectionDegrees = 45;
-    else if (vaneValue > 600)   // east
+    else if (vaneValue > 600) // east
         vaneDirectionDegrees = 90;
-    else if (vaneValue > 500)   // north west
+    else if (vaneValue > 500) // north west
         vaneDirectionDegrees = 315;
-    else if (vaneValue > 350)   // south east
+    else if (vaneValue > 350) // south east
         vaneDirectionDegrees = 135;
-    else if (vaneValue > 200)   // west
+    else if (vaneValue > 200) // west
         vaneDirectionDegrees = 270;
-    else if (vaneValue > 100)   // south west
+    else if (vaneValue > 100) // south west
         vaneDirectionDegrees = 225;
-    else                        // south
+    else // south
         vaneDirectionDegrees = 180;
 
     return vaneDirectionDegrees;
@@ -368,7 +377,7 @@ void rainInterrupt()
 void windInterrupt()
 {
     volatile unsigned long currentMillis = millis();
-    volatile long diffTime = (currentMillis - previousWindInterruptTime);
+    volatile long diffTime               = (currentMillis - previousWindInterruptTime);
 
     if (diffTime > interruptSleep)
     {
@@ -379,7 +388,7 @@ void windInterrupt()
             ++gustCounter;
         else
         {
-            gustCounter = 1;
+            gustCounter           = 1;
             previousGustResetTime = currentMillis;
         }
 
